e05f1a4fb4d19af21698005189ffcfa02a56a34a
Arya Sumant <studywithpwno.1@gmail.com>
2025-11-08 23:59:00 +0530

    auto update 2025-11-08 23:59:00

--- jarvis.py
+++ jarvis.py
@@ -84,7 +84,7 @@ if __name__ == "__main__":
             codePath = "/Applications/iMovie.app"
             subprocess.call(["open", codePath])
 
-        elif 'email to sumant' in query:
+        elif 'email to ' in query:
             try:
                 speak("what should i say?")
                 content = takeCommand()
