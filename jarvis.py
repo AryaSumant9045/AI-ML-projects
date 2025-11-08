cd9fc39b985ae222bbcf8fed70894b2034cf8a14
Arya Sumant <studywithpwno.1@gmail.com>
2025-11-08 23:21:55 +0530

    auto update 2025-11-08 23:21:55

--- jarvis.py
+++ jarvis.py
@@ -71,5 +71,5 @@ if __name__ == "__main__":
             speak(f"Sir, the time is {strTime}")
 
         elif 'open vs code' in query:
-            codePath = "/Applications/"
+            codePath = "/Applications/iMovie"
             os.listdir(codePath)
